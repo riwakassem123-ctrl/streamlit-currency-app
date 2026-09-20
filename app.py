0d93d7662faef76502a81137ed4c037870cd2ab7
riwakassem123-ctrl <riwakassem123@gmail.com>
2026-09-20 15:04:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
+import os
 st.set_page_config(page_title="Lebanon Exchange Rate", layout="wide")
 
 st.title("Lebanon Exchange Rate Analysis")
